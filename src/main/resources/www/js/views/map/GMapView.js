6d185bf6a1f3c8552678c3a92fd81c84f2e36a92
SCadilhac <sylv@netfishers.onl>
2016-01-10 13:15:09 +0100

    Properly displays the info and bubble boxes on GMap routers, and some clean up.

--- src/main/resources/www/js/views/map/GMapView.js
+++ src/main/resources/www/js/views/map/GMapView.js
@@ -8,23 +8,21 @@ define([
 	'models/network/NetworkModel',
 	'models/network/RouterCollection',
 	'models/network/LinkCollection',
-	//'models/network/InterfaceCollection',
+	'models/network/InterfaceCollection',
 	'models/network/Ipv4IgpRouteCollection',
-	'text!templates/map/gmap.html',
-	'text!templates/map/gmapRouter.html',
-	'text!templates/map/routerInterfaces.html',
-	'text!templates/map/routerIpv4IgpRoutes.html',
-	'text!templates/map/interfaceRow.html',
-	'text!templates/map/ipv4IgpRouteRow.html',
-	'text!templates/map/link.html',
-	//'views/map/RouterView',
-	//'views/map/LinkView',
+	'text!templates/gmap/gmap.html',
+	'text!templates/gmap/mapRouter.html',
+	'text!templates/gmap/bubbleRouter.html',
+	'text!templates/gmap/bubbleInterfaces.html',
+	'text!templates/gmap/bubbleIpv4IgpRoutes.html',
+	'text!templates/gmap/bubbleInterfaceRow.html',
+	'text!templates/gmap/bubbleIpv4IgpRouteRow.html',
 	'bootstrap',
 	'infobox',
 	'infobubble'
-], function($, _, Backbone, NetworkModel, RouterCollection, LinkCollection, Ipv4IgpRouteCollection,  
-		gMapTemplate, routerTemplate, interfacesTemplate, ipv4IgpRoutesTemplate, 
-		interfaceRowTemplate, ipv4IgpRouteRowTemplate, linkTemplate) {
+], function($, _, Backbone, NetworkModel, RouterCollection, LinkCollection, InterfaceCollection, Ipv4IgpRouteCollection,  
+		gMapTemplate, routerTemplate, bubbleRouterTemplate, bubbleInterfacesTemplate,
+		bubbleIpv4IgpRoutesTemplate, bubbleInterfaceRowTemplate, bubbleIpv4IgpRouteRowTemplate) {
 	
 	return Backbone.View.extend({
 
@@ -32,60 +30,59 @@ define([
 
 		template: _.template(gMapTemplate),
 		routerTemplate: _.template(routerTemplate),
-		interfacesTemplate: _.template(interfacesTemplate),
-		ipv4IgpRoutesTemplate: _.template(ipv4IgpRoutesTemplate),
-		interfaceRowTemplate: _.template(interfaceRowTemplate),
-		ipv4IgpRouteRowTemplate: _.template(ipv4IgpRouteRowTemplate),
-		linkTemplate: _.template(linkTemplate),
+		bubbleRouterTemplate: _.template(bubbleRouterTemplate),
+		bubbleInterfacesTemplate: _.template(bubbleInterfacesTemplate),
+		bubbleIpv4IgpRoutesTemplate: _.template(bubbleIpv4IgpRoutesTemplate),
+		bubbleInterfaceRowTemplate: _.template(bubbleInterfaceRowTemplate),
+		bubbleIpv4IgpRouteRowTemplate: _.template(bubbleIpv4IgpRouteRowTemplate),
+		
+		defaultBoxOptions: {
+			disableAutoPan: true,
+			maxWidth: 0,
+			pixelOffset: new google.maps.Size(0, -20),
+			zIndex: null,
+			boxStyle: { 
+				background: 'rgb(255,255,255)',
+				color: 'rgb(10,10,10)',
+				'font-size': '13px',
+				opacity: 1,
+				width: "auto"
+			},
+			closeBoxMargin: "10px 2px 2px 2px",
+			closeBoxURL: "",
+			infoBoxClearance: new google.maps.Size(1, 1),
+			isHidden: false,
+			pane: "floatPane",
+			enableEventPropagation: false
+		},
+		
+		defaultBubbleOptions: {
+			//maxWidth: 300, 
+			//maxHeight: 300,
+			shadowStyle: 1,
+			padding: 5,
+			backgroundColor: 'rgb(220,220,220)',
+			borderRadius: 2,
+			arrowSize: 10,
+			borderWidth: 1,
+			borderColor: '#2c2c2c',
+			disableAutoPan: true,
+			hideCloseButton: false,
+			closeSrc: 'img/fileclose12.png',
+			arrowPosition: 20,
+			//backgroundClassName: 'phoney',
+			arrowStyle: 2
+		},
+		
+		defaultRouterIcon: {
+			url: '../../../img/router_grey.svg',
+			scaledSize: new google.maps.Size(30, 30),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(20, 20)
+		},
 		
 		initialize: function(){
 			var that = this ;
-			
-			this.myInfoBoxOptions = {
-					disableAutoPan: true,
-					maxWidth: 0,
-					pixelOffset: new google.maps.Size(0, -20),
-					zIndex: null,
-					boxStyle: { 
-						  background: 'rgb(255,255,255)',
-						  color: 'rgb(10,10,10)',
-						  'font-size': '13px',
-						  opacity: 1,
-						  width: "auto"
-					},
-					closeBoxMargin: "10px 2px 2px 2px",
-					closeBoxURL: "",
-					infoBoxClearance: new google.maps.Size(1, 1),
-					isHidden: false,
-					pane: "floatPane",
-					enableEventPropagation: false
-			};
-			
-			this.myInfoBubbleOptions = {
-			      //maxWidth: 300, 
-			      //maxHeight: 300,
-			      shadowStyle: 1,
-			      padding: 5,
-			      backgroundColor: 'rgb(220,220,220)',
-			      borderRadius: 2,
-			      arrowSize: 10,
-			      borderWidth: 1,
-			      borderColor: '#2c2c2c',
-			      disableAutoPan: true,
-			      hideCloseButton: false,
-			      closeSrc: 'img/fileclose12.png',
-			      arrowPosition: 20,
-			      //backgroundClassName: 'phoney',
-			      arrowStyle: 2	
-			};
-			
-			this.icon = {
-				  	url: '../../../img/router_grey.svg',
-					scaledSize: new google.maps.Size(30, 30),
-					origin: new google.maps.Point(0,0),
-					anchor: new google.maps.Point(20,20)
-			};
-			
 			
 			this.network = new NetworkModel({
 				id: this.id
@@ -102,7 +99,6 @@ define([
 			this.links.on("add", this.onAddedLink, this);
 			this.links.on("change", this.onChangedLink, this);
 			this.links.on("remove", this.onDestroyedLink, this);
-          
 			
 			Backbone.history.on("route", function (route, router) {
 				if (that.autoRefreshInt !== false && that.autoRefreshInt != null) {
@@ -110,9 +106,9 @@ define([
 				}
 			});
 			this.mapOptions = {
-					mapTypeId: google.maps.MapTypeId.TERRAIN
+				mapTypeId: google.maps.MapTypeId.TERRAIN
 			};
-			this.bounds = new google.maps.LatLngBounds();	
+			this.bounds = new google.maps.LatLngBounds();
 			
 			this.network.fetch().done(function() {
 				that.render();
@@ -138,11 +134,11 @@ define([
 			var that = this;
 			if (this.autoRefreshInt === false || this.autoRefreshInt == null) {
 				this.autoRefreshInt = setInterval(function() {
-				  that.$("#gmap-toolbar #refresh").prop('disabled', true);
-				  that.refresh();
+					that.$("#gmap-toolbar #refresh").prop('disabled', true);
+					that.refresh();
 				}, 1000 * 3);
 			} else {
-			clearInterval(this.autoRefreshInt);
+				clearInterval(this.autoRefreshInt);
 				this.autoRefreshInt = false;
 			}
 		},
@@ -150,101 +146,56 @@ define([
 		onAddedRouter: function(router, routers) {
 			var that = this;
 			
-			var routerHtml = this.routerTemplate(router.toJSON());
-			
-			//var interfacesHtml = this.interfacesTemplate(router.toJSON());
-          	var interfacesHtml = '<table><thead>'+
-				'<tr><th>Interface</th><th>Description</th><th>IP address</th><th></th></tr></thead>';
-			          
-          	var itfArray = router.get('ipv4Interfaces');
-          	$.each(itfArray, function(i,v) {
-              		interfacesHtml += that.interfaceRowTemplate(v);
-            }); 
-          	interfacesHtml += '</table>';
-          	
-         	var ipv4IgpRoutesHtml = this.ipv4IgpRoutesTemplate(router.toJSON());
-          
-			this.ipv4IgpRoutes = new Ipv4IgpRouteCollection([], {
-				network: this.network.get('id'),
-				router: router.get('id')
+			var position = new google.maps.LatLng(router.get('latitude'), router.get('longitude'));
+			this.bounds.extend(position);
+			this.map.fitBounds(this.bounds);
+			var marker = new google.maps.Marker({
+				position: position,
+				map: this.map,
+				animation: google.maps.Animation.DROP,
+				icon: this.defaultRouterIcon,
+				routerId: router.get('id')
 			});
-			//console.log(this.ipv4IgpRoutes);
-			this.ipv4IgpRoutes.each(function(routerIpv4IgpRoute) {
-				var now = Date.now();
-				var data = routerIpv4IgpRoute.toJSON();
-				data.intf = routerIpv4IgpRoute;
+			marker.addListener('click', function(e) {
+				var marker = this;
+				var router = that.routers.get(this.routerId);
+				if (!router) return;
+				var ipv4IgpRoutes = new Ipv4IgpRouteCollection([], {
+					network: that.network.get('id'),
+					router: router.get('id')
+				});
+				ipv4IgpRoutes.fetch().done(function() {
+					var bubble = new InfoBubble(that.defaultBubbleOptions);
+					bubble.addTab("Router", that.bubbleRouterTemplate(router.toJSON()));
+					var $interfacesTab = $("<div>").append(that.bubbleInterfacesTemplate(router.toJSON()));
+					_.each(router.get("ipv4Interfaces"), function(routerInterface) {
+						$interfacesTab.find("tbody").append($(that.bubbleInterfaceRowTemplate(routerInterface)));
+					});
+					bubble.addTab("Interfaces", $interfacesTab.html());
+					var $routesTab = $("<div>").append(that.bubbleIpv4IgpRoutesTemplate(router.toJSON()));
+					ipv4IgpRoutes.each(function(route) {
+						$routesTab.find("tbody").append($(that.bubbleIpv4IgpRouteRowTemplate(route.toJSON())));
+					});
+					bubble.addTab("Prefixes", $routesTab.html());
+					bubble.open(that.map, marker);
+				});
 				
-				var ageMonths = typeof(data.date) != 'undefined' ? parseInt(((now - data.date)/(1000*60*60*24*30))%12) : 0;
-				var ageDays = typeof(data.date) != 'undefined' ? parseInt(((now - data.date)/(1000*60*60*24))%30) : 0;
-				//console.log("data.date: "+data.date+" ageMonths: "+ageMonths+" ageDays: "+ageDays);
-				var age = '';
-				if (ageMonths > 0) {
-					age += ageMonths+"m ";
-				}
-				if (ageDays > 0) {
-					age += ageDays+"d ";
-				}
-				age +=
-					("00"+parseInt(((now - data.date)/(1000*60*60))%24)+":").slice(-3)+
-					("00"+parseInt(((now - data.date)/(1000*60))%60)+":").slice(-3)+
-					("00"+parseInt((now - data.date)/1000)%60).slice(-2);
-				data.age = age;
 				
-				var bgColor = '';
-				if (data.justNew === true) {
-					bgColor = 'bgcolor=#a2dfa3';
-				}
-				else if (data.justLost === true) {
-					bgColor = 'bgcolor=#ffaeae';
-				}
-				data.bgColor = bgColor;
-				
-				ipv4IgpRoutesHtml.append(that.ipv4IgpRouteRowTemplate(data));
 			});
-
-			this.pos = new google.maps.LatLng(router.get('latitude'), router.get('longitude'));
-	  		this.bounds.extend(this.pos);
-	  		this.map.fitBounds(this.bounds);
-	  		
-	  		marker = new google.maps.Marker({
-	    		position: this.pos,
-	    		map: this.map,
-	    		animation: google.maps.Animation.DROP,
-	    		icon: this.icon
-	    	});
-          
-          			
-	  		this.mapsInfoBox2Router(marker,router.get('name'),this.map);
-	  		this.mapsInfoBubble2Router(marker, routerHtml, interfacesHtml, ipv4IgpRoutesHtml, this.map);
-	        		
-		},
-      	
-		
-		mapsInfoBox2Router: function(rtr, content, map) {
-		    var infoBox = new InfoBox(this.myInfoBoxOptions);
-		  	google.maps.event.addListener(rtr, 'mouseover', function() {
-		  		infoBox.setContent(content);
-		  		infoBox.open(map, rtr);
+			marker.addListener('mouseover', function() {
+				var router = that.routers.get(this.routerId);
+				if (!router) return;
+				this.box = new InfoBox(that.defaultBoxOptions);
+				this.box.setContent(that.routerTemplate(router.toJSON()));
+				this.box.open(that.map, this);
 			});
-			google.maps.event.addListener(rtr, 'mouseout', function() {
-				infoBox.close(true);
+			marker.addListener('mouseout', function() {
+				if (typeof this.box === "object") {
+					this.box.close(true);
+					delete(this.box);
+				}
 			});
 		},
-		
-		mapsInfoBubble2Router: function(m, rtr, itf, routes, map) {
-			  //console.log("loads the function for:"+rtr);
-			  var  infoBubble = new InfoBubble(this.myInfoBubbleOptions);
-			  infoBubble.addTab('Router', rtr);
-			  infoBubble.addTab('Interfaces', itf);
-			  infoBubble.addTab('Prefixes', routes);
-			  infoBubble.setTabActive(0);
-			  /*infoBubble.updateTab(0,'Router', router_infos);
-				infoBubble.updateTab(1,'Interfaces', interfaces);
-				infoBubble.updateTab(2,'Prefixes', routes);*/
-			  google.maps.event.addListener(m, 'click', function() {
-		        infoBubble.open(map, m);
-			  });	
-		},		
 		
 		onChangedRouter: function(router, routers) {
 		},
@@ -256,8 +207,6 @@ define([
 			var that = this;
 			var source = null;
 			var target = null;
-          
-          	//console.log(link);
 			
 			this.routers.each(function(router) {
 				if (link.get('localRouter').identifier == router.get('routerId').identifier) {
@@ -269,8 +218,8 @@ define([
 			});
 			if (source != null && target != null) {
 				var path = [
-							{lat: source.get('latitude'), lng: source.get('longitude')},
-							{lat: target.get('latitude'), lng: target.get('longitude')}
+					{lat: source.get('latitude'), lng: source.get('longitude')},
+					{lat: target.get('latitude'), lng: target.get('longitude')}
 				];
 				var line = new google.maps.Polyline({
 					path: path,
@@ -302,17 +251,17 @@ define([
 						source.get('name')+" and: "+target.get('name')+'</small>', 
 						this.map
 				);
-              	
+				
 				this.mapsInfoBubble2Link(shadowLine,linkContent, this.map);
 			}
 		},
 		
 		mapsInfoBox2Link: function(line, content, map) {
-		  	var infoBox = new InfoBox(this.myInfoBoxOptions);
-		  	google.maps.event.addListener(line, 'mouseover', function(evt) {
-		  		infoBox.setContent(content);
-		  		infoBox.setPosition(evt.latLng);
-		  		infoBox.open(map);
+			var infoBox = new InfoBox(this.defaultBoxOptions);
+			google.maps.event.addListener(line, 'mouseover', function(evt) {
+				infoBox.setContent(content);
+				infoBox.setPosition(evt.latLng);
+				infoBox.open(map);
 			});
 			google.maps.event.addListener(line, 'mouseout', function(evt) {
 				infoBox.close(true);
@@ -320,12 +269,12 @@ define([
 		},
 		
 		mapsInfoBubble2Link: function(line, content, map) {
-			  var  infoBubble = new InfoBubble(this.myInfoBubbleOptions);
-			  google.maps.event.addListener(line, 'click', function(evt) {
-				  infoBubble.setContent(content);
-				  infoBubble.setPosition(evt.latLng);
-				  infoBubble.open(map);
-			  });	
+			var  infoBubble = new InfoBubble(this.myInfoBubbleOptions);
+			google.maps.event.addListener(line, 'click', function(evt) {
+				infoBubble.setContent(content);
+				infoBubble.setPosition(evt.latLng);
+				infoBubble.open(map);
+			});	
 		},
 		
 		onChangedLink: function(link) {
@@ -340,7 +289,7 @@ define([
 			this.$el.show().html(this.template());
 			
 			this.map = new google.maps.Map(document.getElementById('gmap'), this.mapOptions);
-          			
+			
 			this.$("#gmap-toolbar #refresh").click(function() {
 				that.$("#gmap-toolbar #refresh").prop('disabled', true);
 				that.refresh();
